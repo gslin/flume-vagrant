1fdcd7b243cb15f2973ee0c6f1e4f8428738ef24
Gea-Suan Lin <gslin@gslin.org>
2014-05-19 06:17:28 +0800

    Basic install.sh.

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+
+work() {
+    sed -i -e 's@http://archive.ubuntu.com@http://tw.archive.ubuntu.com@' /etc/apt/sources.list
+
+    apt-get update
+
+    DEBIAN_FRONTEND=noninteractive apt-get -y install git vim-nox
+}
+
+work
