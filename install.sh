d19d06f9c666ce45f999fe1de1ff65dd7224f678
Gea-Suan Lin <gslin@gslin.org>
2014-05-19 06:19:19 +0800

    Add cloudera repository.

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 work() {
+    curl -s http://archive.cloudera.com/cdh5/ubuntu/lucid/amd64/cdh/archive.key | apt-key add -
     sed -i -e 's@http://archive.ubuntu.com@http://tw.archive.ubuntu.com@' /etc/apt/sources.list
+
+    cat > /etc/apt/sources.list.d/cloudera.list <<EOF
+deb [arch=amd64] http://archive.cloudera.com/cdh5/ubuntu/precise/amd64/cdh precise-cdh5 contrib
+deb-src http://archive.cloudera.com/cdh5/ubuntu/precise/amd64/cdh precise-cdh5 contrib
+EOF
 
     apt-get update
 
